eb3381ba806520ceba5d8f28350e74239c33abaf
chriswilsondev910 <77631508+chriswilsondev910@users.noreply.github.com>
2021-06-08 19:09:40 -0600

    fixed one more for deals in seller component

--- src/components/pages/Seller.js
+++ src/components/pages/Seller.js
@@ -50,7 +50,7 @@ export const Seller = ({ seller }) => (
        onPress={() => NavigationService.navigate("dealslist")
        }
       > */}
-      {seller.promo_codes_count == 0 ?
+      {seller.promo_codes_count != undefined && (seller.promo_codes_count == 0 ?
         seller.free_delivery_cutoff != '0.00'  && <View style={styles.details_delivery}>
         <PriceSVG style={styles.dealItem} height={15} width={15}/>
         {/* <AppText style={styles.products_freeDelivery} numberOfLines={1}>Spend {seller.currency.icon}{seller.free_delivery_cutoff} Get Free Delivery */}
@@ -72,7 +72,7 @@ export const Seller = ({ seller }) => (
         <PriceSVG style={styles.dealItem} height={15} width={15}/>
         <AppText style={styles.products_freeDelivery} numberOfLines={1}>{seller.promo_codes_count} Deals Available
         </AppText></View>
-        }
+      )}
         {/* </TouchableOpacity> */}
     </View>
     {/* <AppText style={styles.distance} numberOfLines={1}>
