b0c31380c65eb7691eec93f79e5f6831c088fda4
chriswilsondev910 <77631508+chriswilsondev910@users.noreply.github.com>
2021-06-14 09:46:39 -0600

    share url fixed

--- src/core/config.js
+++ src/core/config.js
@@ -1,7 +1,7 @@
 export const Config = {
-  shareURL: ' https://chowlocal.com',
+  shareURL: 'https://chowlocal.com',
   signUpURL: 'https://live.chowlocal.com/start-selling',
-  // apiBaseURL: 'https://account.chowlocal-dev.com/api/v1',
+  //apiBaseURL: 'https://account.chowlocal-dev.com/api/v1',
   apiBaseURL: 'https://live.chowlocal.com/api/v1',
   //apiBaseURL: 'https://live.shopl.net/api/v1',
   followSHOPL: 'https://www.facebook.com/SHOPLApp/',
